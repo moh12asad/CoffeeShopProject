924a0bb4f56ec324b76023f7cf2901ac649692bf
moh12asad <moh12asad@hotmail.com>
2022-01-19 22:50:38 +0200

    Add project files.

---
+++ CoffeeShopProject/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CoffeeShopProject.Models
+{
+    public class User
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public string Vip { get; set; }
+        [Required]
+        public string UserType { get; set; }
+        public string credit { get; set; }
+
+        public string pin { get; set; }
+    }
+}
---
+++ CoffeeShopProject/UserDal/UserDal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using CoffeeShopProject.Models;
+
+namespace CoffeeShopProject.UserDal
+{
+
+    public class UserDal:DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().ToTable("User");
+        }
+        public DbSet <User> Users { get; set; }
+
+    }
+}
